3538e9a52db05cff561b24db6f44133515872926
Junior-Puerta <junior_masson@hotmail.com>
2024-10-30 18:50:16 -0300

    codigo inicial

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT, ()=>{
-console.log("Servidor escutando")
-})
+app.listen(PORT, () => {
+  console.log("Servidor escutando");
+});
+
+app.get("/coisas/carros", (req, res) =>
+  res.send("Você está no diretório /coisas/carros")
+);
+
+app.get("/coisas/carros/:CarroId", (req, res) => {});
+
+app.post("/coisas/carros", (req, res) => {});
+
+app.put("/coisas/carros/CarroId", (req, res) => {});
